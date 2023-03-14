0172749f7e333bee621bbaa9e2bc5cf4754633f2
Sinue <117844774+sifusinmorir@users.noreply.github.com>
2023-03-14 10:39:31 -0600

    Add files via upload

---
+++ EVA2_7_THIS/src/eva2_7_this/EVA2_7_THIS.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva2_7_this;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA2_7_THIS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+class Persona{
+    private String nombre;
+    private String apellido;
+
+        public Persona(String nombre, String apellido) {
+            this.nombre = nombre;
+            this.apellido = apellido;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public void setNombre(String nombre) {
+            this.nombre = nombre;
+        }
+
+        public String getApellido() {
+            return apellido;
+        }
+
+        public void setApellido(String apellido) {
+            this.apellido = apellido;
+        }
+    
+}
+}
